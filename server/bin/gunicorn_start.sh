d900cd433d8fcf337dd2c3a915e58565132e508a
Eriq Augustine <eaugusti@chromium.org>
2013-12-25 03:44:07 -0800

    Made a working systemd unit for gunicorn and changed the startup script a bit. Added all of the working units to the config directory.

--- server/bin/gunicorn_start.sh
+++ server/bin/gunicorn_start.sh
@@ -11,6 +11,8 @@ NUM_WORKERS=1  # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=media_server.settings  # which settings file should Django use
 DJANGO_WSGI_MODULE=media_server.wsgi  # WSGI module name
 
+PID_FILE=$PROJECT_ROOT/run/gunicorn.pid
+
 ACCESS_LOG=$PROJECT_ROOT/logs/gunicorn-access.log
 ERROR_LOG=$PROJECT_ROOT/logs/gunicorn-error.log
 
@@ -22,13 +24,14 @@ source $PROJECT_ROOT/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
-# Create the run directory if it doesn't exist
+# Create the run directory if it doesn't exist.
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
-# Start your Django Unicorn
-# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec $PROJECT_ROOT/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+# Remove the pid file (if it exists).
+test -f $PID_FILE && rm -f $PID_FILE
+
+$PROJECT_ROOT/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
@@ -36,5 +39,8 @@ exec $PROJECT_ROOT/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --log-level debug \
   --access-logfile $ACCESS_LOG \
   --error-logfile $ERROR_LOG \
-  --bind=unix:$SOCKFILE
-#  --worker-class gunicorn.workers.ggevent.GeventWorker \
+  --bind=unix:$SOCKFILE \
+  --pid $PID_FILE \
+  --daemon
+
+sleep 1
